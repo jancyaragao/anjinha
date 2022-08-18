610a8c6d81af56975d5322a8977872eb57869657
Jâncy Aragão <jancy.aragao@gmail.com>
2022-08-18 15:49:23 -0300

    Remodelação da página com nova estrutura de paginação

--- main.js
+++ main.js
@@ -1,20 +1,35 @@
-let prev = document.getElementById("prev");
-let next = document.getElementById("next");
+$(function() {
+    var $mybook = $('#mybook');
+    var $bttn_next = $('#next_page_button');
+    var $bttn_prev = $('#prev_page_button');
+    var $loading = $('#loading');
+    var $mybook_images = $mybook.find('img');
+    var cnt_images = $mybook_images.length;
+    var loaded = 0;
 
-prev.addEventListener("click", prevImg);
-next.addEventListener("click", nextImg);
+    $mybook_images.each(function() {
+        var $img = $(this);
+        var source = $img.attr('src');
+        $('<img/>').load(function() {
+            ++loaded;
+            if (loaded == cnt_images) {
+                $loading.hide();
+                $bttn_next.show();
+                $bttn_prev.show();
+                $mybook.show().booklet({
+                    name: null, //  
+                    width: 800, //  
+                    height: 500, //   
+                    speed: 600, //  
+                    direction: 'LTR', //  
+                    //  
+                    next: $bttn_next, //  
+                    prev: $bttn_prev, //  
 
+                });
+                Cufon.refresh();
+            }
+        }).attr('src', source);
+    });
 
-let second = document.getElementById('second');
-
-function prevImg() {
-    second.style.msTransform = "rotateY(0deg)";
-    second.style.webkitTransform = "rotateY(0deg)";
-    second.style.transform = "rotateY(0deg)";
-}
-
-function nextImg() {
-    second.style.msTransform = "rotateY(-180deg)";
-    second.style.webkitTransform = "rotateY(-180deg)";
-    second.style.transform = "rotateY(-180deg)";
-}
+});
